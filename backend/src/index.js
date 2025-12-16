24814faff88500c4ba09ee04687d424ab735c02f
Sunil Kumar <sunilroy450@gmail.com>
2025-12-16 13:16:22 +0530

    feat: add folder basic structure and init express, setup main file

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
